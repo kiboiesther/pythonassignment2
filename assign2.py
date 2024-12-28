8029fceb5c9d0c440ea0cddae37f07edf46cffa8
kiboiesther <kiboiesther200@gmail.com>
2024-12-28 12:48:03 +0300

    assignmemt 2

---
+++ assign2.py
@@ -0,0 +1,26 @@
+#question 1
+my_list = []
+
+#question 2
+my_list.extend([10, 20, 30, 40])
+print(my_list)
+
+#question 3
+my_list.insert(1, 15)
+print(my_list)
+
+#question 4
+my_list.extend([50, 60, 70])
+print(my_list)
+
+#question 5
+my_list.remove(70)
+print(my_list)
+
+#question 6
+my_list.sort()
+print(my_list)
+
+#question 7
+index_of_30 = my_list.index(30) 
+print("Index of 30:", index_of_30)
